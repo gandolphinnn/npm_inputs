f2fb4669188e7953b2475efdbff49c5fdade639f
gandolphinnn <gandolfiluca03@gmail.com>
2024-01-17 18:11:07 +0100

    WIP class Button

--- src/index.ts
+++ src/index.ts
@@ -1,57 +1,117 @@
-import { Singleton } from '@gandolphinnn/utils';
+import { Singleton, clamp, coalesce } from '@gandolphinnn/utils';
 import { Coord } from '@gandolphinnn/graphics2';
 
+enum BtnState {
+	Up, Down, Hold, Released, Dbl
+}
+class Button {
+	private _state: BtnState;
+	get state() { //TODO this getter update the state based on the last update
+		return this._state
+	}
+	lastUpdate: Date;
+	setState(state: BtnState.Up | BtnState.Down ) {
+		const currentVal = this.state; //MUST PICK the getter
+		switch (state) {
+			case BtnState.:
+				
+				break;
+		
+			default:
+				break;
+		}
+
+		const BTN_STATE_FLOW = {
+			[BtnState.Up]: {
+				[BtnState.Up]: BtnState.Up,
+				[BtnState.Down]: BtnState.Down,
+			},
+			[BtnState.Down]: {
+				[BtnState.Up]: BtnState.Released,
+				[BtnState.Down]: BtnState.Hold,
+			},
+			[BtnState.Hold]: {
+				[BtnState.Up]: BtnState.Released,
+				[BtnState.Down]: BtnState.Hold,
+			},
+			[BtnState.Released]: {
+				[BtnState.Up]: BtnState.Up,
+				[BtnState.Down]: BtnState.Dbl,
+			},
+			[BtnState.Dbl]: {
+				[BtnState.Up]: BtnState.Released,
+				[BtnState.Down]: BtnState.Hold,
+			},
+		}
+	}
+}
 export class Input extends Singleton {
 	private static get get() { return this.singletonInstance as Input }
 
-	private _mouse: {
-		pos: Coord,
-		btn: {
-			l: boolean,
-			m: boolean,
-			r: boolean
-		}
-	}
-	private _key: Record<string, boolean>;
+	private _mouse: { pos: Coord, wheel: Coord, btn: Record<number, Button> }
+	private _key: Record<string, Button>;
 
-	static get mouse() { return Input.get._mouse }
-	static get key() { return Input.get._key }
+	preventDef: boolean;
+	allowF5F12: boolean;
+
 	constructor() {
 		super();
-		this._mouse = {pos: new Coord(0,0), btn: {l: false, m: false, r: false}};
+		this._mouse = {pos: new Coord(0,0), wheel: new Coord(0, 0), btn: {}};
 		this._key = {};
-	}
-	static init() {
-		this.singletonInstance as Input
-		document.addEventListener('contextmenu', e => e.preventDefault()); //? prevent right click menu
+		this.preventDef = true;
+		this.allowF5F12 = true;
+
+		document.addEventListener('contextmenu', e => this.preventeDefault(e)); //? prevent right click menu
+		document.addEventListener('mousemove', e => {
+			Input.get._mouse.pos = new Coord(e.clientX, e.clientY);
+		});
+		document.addEventListener('wheel', e => {
+			this.preventeDefault(e);
+			//TODO turn -0 in 0
+			Input.get._mouse.wheel = new Coord(clamp(e.deltaX, -1, 1), clamp(e.deltaY, -1, 1));
+		});
 		document.addEventListener('mousedown', e => {
-			switch (e.button) {
-				case 0: Input.mouse.btn.l = true; break;
-				case 1: Input.mouse.btn.m = true; e.preventDefault(); break; //? prevent middle click scroller
-				case 2: Input.mouse.btn.r = true; break;
-				default: break;
-			}
+			/*this.preventeDefault(e);
+			Input.get._mouse.btn[e.button] = BtnState.Down;*/
 		})
 		document.addEventListener('mouseup', e => {
-			switch (e.button) {
-				case 0: Input.mouse.btn.l = false; break;
-				case 1: Input.mouse.btn.m = false; break;
-				case 2: Input.mouse.btn.r = false; break;
-				default: break;
-			}
+			//Input.get._mouse.btn[e.button] = BtnState.Up;
 		})
-		document.addEventListener('mousemove', e => {
-			Input.mouse.pos.y = e.clientY;
-			Input.mouse.pos.x = e.clientX;
-		});
 		document.addEventListener('keydown', e => {
-			//if (e.code != 'F5' && e.code != 'F12') e.preventDefault() //? allow to refresh and to open the page inspect
-			let code = e.code.replace('Key', 'K').replace('Digit', 'D').replace('Numpad', 'P').replace('Arrow', '');
-			Input.key[code] = true;
+			if (!(this.allowF5F12 && (e.code == 'F5' || e.code == 'F12'))) //TODO clean this condition
+				this.preventeDefault(e) //? allow to refresh and to open the page inspect
+
+			const code = replaceKeyCode(e.code);
+			this.changeBtnState(code, 1);
+			console.log(Input.get._key[code].state);
 		});
 		document.addEventListener('keyup', e => {
-			let code = e.code.replace('Key', 'K').replace('Digit', 'D').replace('Numpad', 'P').replace('Arrow', '');
-			delete Input.key[code];
+			const code = replaceKeyCode(e.code);
+			this.changeBtnState(code, 0);
+			console.log(Input.get._key[code].state);
 		});
-	};
+	}
+	private preventeDefault(e: Event) {
+		if (this.preventDef)
+			e.preventDefault();
+	}
+	private changeBtnState(code: string, isDown: 0 | 1) {
+		const currentVal = coalesce(Input.get._key[code], BtnState.Up) as BtnState
+		Input.get._key[code] = BTN_STATE_FLOW[currentVal][isDown] as BtnState
+	}
+	static mousePos() {
+		return Input.get._mouse.pos.copy();
+	}
+	static mouseBtnState(btnIndex: number) {
+		return Input.get._mouse.btn[btnIndex];
+	}
+	static inputSamples() {
+		//TODO 
+	}
+}
+export function getTimeStamp() {
+	return new Date().getMilliseconds();
+}
+export function replaceKeyCode(code: string) {
+	return code.replace('Key', 'K').replace('Digit', 'D').replace('Numpad', 'P').replace('Arrow', '');
 }
--- src/test.ts
+++ src/test.ts
@@ -1,3 +1,13 @@
-import { Input } from './index.js';
+import { MainCanvas } from '@gandolphinnn/graphics2';
+import { Input, getTimeStamp } from './index.js';
 
-Input.init();
+console.log(Input.mousePos());
+
+function animate() {
+	requestAnimationFrame(animate);
+	//ctx.clearRect(0,0, innerWidth, innerHeight);
+	//console.log(Input.mousePos(), Input.mouseBtnState(0));
+
+}
+animate();
+/**/

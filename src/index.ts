637f8348537fdddbdf1c9186cde6be40b7b19f98
Luca Gandolfi <gandolfiluca03@gmail.com>
2024-02-19 01:38:58 +0100

    Log Btns, Timer, Mouse isInside

--- src/index.ts
+++ src/index.ts
@@ -5,27 +5,64 @@ import { Coord, MainCanvas } from '@gandolphinnn/graphics2';
 export enum BtnState {
 	Up, Down, Released, Hold, Dbl
 }
+export type WheelState = 0 | 1 | -1;
 export type KeyCode = 'Backspace' | 'Tab' | 'Enter' | 'ShiftLeft' | 'ShiftRight' | 'ControlLeft' | 'ControlRight' | 'AltLeft' | 'AltRight' | 'Pause' | 'CapsLock' | 'Escape' | 'Space' | 'PageUp' | 'PageDown' | 'End' | 'Home' | 'Left' | 'Up' | 'Right' | 'Down' | 'PrintScreen' | 'Insert' | 'Delete' | 'D0' | 'D1' | 'D2' | 'D3' | 'D4' | 'D5' | 'D6' | 'D7' | 'D8' | 'D9' | 'Quote' | 'KA' | 'KB' | 'KC' | 'KD' | 'KE' | 'KF' | 'KG' | 'KH' | 'KI' | 'KJ' | 'KK' | 'KL' | 'KM' | 'KN' | 'KO' | 'KP' | 'KQ' | 'KR' | 'KS' | 'KT' | 'KU' | 'KV' | 'KW' | 'KX' | 'KY' | 'KZ' | 'MetaLeft' | 'MetaRight' | 'ContextMenu' | 'P0' | 'P1' | 'P2' | 'P3' | 'P4' | 'P5' | 'P6' | 'P7' | 'P8' | 'P9' | 'PMultiply' | 'PAdd' | 'PSubtract' | 'PDecimal' | 'PDivide' | 'F1' | 'F2' | 'F3' | 'F4' | 'F5' | 'F6' | 'F7' | 'F8' | 'F9' | 'F10' | 'F11' | 'F12' | 'NumLock' | 'ScrollLock' | 'Semicolon' | 'Equal' | 'Comma' | 'Minus' | 'Period' | 'Slash' | 'Backquote' | 'BracketLeft' | 'Backslash' | 'BracketRight'
-export const MS_DELAY_DOWNDBL_HOLD = 300;
-export const MS_DELAY_RELEASED_UP = 300;
-const EVENT_BTNSTATE = { //! NO EXPORT
+
+export const MS_DELAY_BTN = {
+	[BtnState.Up]: 300, //? Delay from Button.Released to Button.Up
+	[BtnState.Down]: 400 //? Delay from Button.Down or Button.Dbl to Button.Hold
+}
+export const MS_DELAY_WHEEL_RESET: number = 300; //? Delay from Wheel.x or Wheel.y == -1 or 1 to == 0
+
+const EVENT_BTNSTATE = {
 	[BtnState.Up]: [BtnState.Up, BtnState.Released],
 	[BtnState.Down]: [BtnState.Down, BtnState.Hold, BtnState.Dbl]
 }
 //#endregion
 
 //#region Classes
-export class Button {
+//todo i stil dont know if this class is useful
+class Timer {
+	protected _timeStamp: number;
+	protected get elapsed() {
+		return getTodayTimeStamp() - this._timeStamp;
+	}
+	constructor() {
+		this._timeStamp = getTodayTimeStamp();
+	}
+}
+class WheelAxis extends Timer {
+	private _state: WheelState;
+	get state() {
+		if (this.state !== 0 && this.elapsed >= MS_DELAY_WHEEL_RESET) {
+			this.state = 0;
+		}
+		return this._state;
+	}
+	private set state(state: WheelState) {
+		if (this.state === state) {
+			return;
+		}
+		this._timeStamp = getTodayTimeStamp();
+		this._state = state;
+	}
+	constructor() {
+		super();
+		this._state = 0;
+	}
+}
+//todo extends Timer
+class Button {
 	private _state: BtnState;
 	private lastTimeStamp: number;
 	get elapsedSinceUpdate() {
 		return getTodayTimeStamp() - this.lastTimeStamp;
 	}
 	get state() {
-		if (this.elapsedSinceUpdate >= MS_DELAY_DOWNDBL_HOLD && (this._state == BtnState.Down || this._state == BtnState.Dbl))
+		if (this.elapsedSinceUpdate >= MS_DELAY_BTN[BtnState.Down] && (this._state == BtnState.Down || this._state == BtnState.Dbl))
 			this._state = BtnState.Hold;
 
-		if (this.elapsedSinceUpdate >= MS_DELAY_RELEASED_UP && this._state == BtnState.Released)
+		if (this.elapsedSinceUpdate >= MS_DELAY_BTN[BtnState.Up] && this._state == BtnState.Released)
 			this._state = BtnState.Up;
 
 		return this._state;
@@ -39,7 +76,7 @@ export class Button {
 	}
 	toggle(newState: BtnState.Up | BtnState.Down) {
 		const currentState = this.state; //? MUST PICK the getter
-		if (EVENT_BTNSTATE[newState].indexOf(currentState) != -1) {
+		if (EVENT_BTNSTATE[newState].indexOf(currentState) != -1) { //? If the new State is similar to the current state (like released and up)
 			return;
 		}
 		switch (currentState) {
@@ -54,34 +91,43 @@ export class Button {
 export class Input extends Singleton {
 	private static get get() { return this.singletonInstance as Input }
 
-	private _mouse: { pos: Coord, wheel: {x: number, y: number}, btn: Record<number, Button> }
+	private _mouse: { pos: Coord, wheel: {x: WheelAxis, y: WheelAxis}, btn: Record<number, Button>, isInside: boolean }
 	private _keys: Record<string, Button>;
 
 	notPreventedCodes: string[];
 
 	constructor() {
 		super();
-		this._mouse = {pos: new Coord(0,0), wheel: {x: 0, y: 0}, btn: {}};
+		this._mouse = {pos: new Coord(0,0), wheel: {x: new WheelAxis, y: new WheelAxis}, btn: {}, isInside: null};
 		this._keys = {};
 		this.notPreventedCodes = ['F5', 'F12'];
 
 		//#region Add events
 		MainCanvas.get.cnv.addEventListener('contextmenu', e => e.preventDefault()); //? prevent right click menu
-		MainCanvas.get.cnv.addEventListener('mousemove', e => {
-			Input.get._mouse.pos = new Coord(e.clientX, e.clientY);
-		});
-		
-		MainCanvas.get.cnv.addEventListener('wheel', e => {
+
+		/* MainCanvas.get.cnv.addEventListener('wheel', e => {
 			e.preventDefault(); //TODO figure out how to reset the wheel object
 			Input.get._mouse.wheel.x = clamp(e.deltaX, -1, 1) +1 -1;
 			Input.get._mouse.wheel.y = clamp(e.deltaY, -1, 1) +1 -1;
-		}, {passive: false}); //? passive -> https://chromestatus.com/feature/6662647093133312
+		}, {passive: false}); //? passive -> https://chromestatus.com/feature/6662647093133312 */
+
+		MainCanvas.get.cnv.addEventListener('mousemove', e => {
+			Input.get._mouse.pos.x = e.clientX;
+			Input.get._mouse.pos.y = e.clientY;
+		});
+		
 		MainCanvas.get.cnv.addEventListener('mousedown', e => {
-			/*this.preventeDefault(e);
-			Input.get._mouse.btn[e.button] = BtnState.Down;*/
+			e.preventDefault();
+			this.toggleMouseBtn(e.button, BtnState.Down);
 		})
 		MainCanvas.get.cnv.addEventListener('mouseup', e => {
-			//Input.get._mouse.btn[e.button] = BtnState.Up;
+			this.toggleMouseBtn(e.button, BtnState.Up);
+		})
+		MainCanvas.get.cnv.addEventListener('mouseenter', e => {
+			Input.get._mouse.isInside = true;
+		})
+		MainCanvas.get.cnv.addEventListener('mouseleave', e => {
+			Input.get._mouse.isInside = false;
 		})
 		MainCanvas.get.cnv.addEventListener('keydown', e => {
 			const code = replaceKeyCode(e.code);
@@ -89,30 +135,41 @@ export class Input extends Singleton {
 			if (this.notPreventedCodes.indexOf(code) == -1)
 				e.preventDefault()
 
-			this.toggleKey(code, BtnState.Down);
+			this.toggleKeybtn(code, BtnState.Down);
 		});
 		MainCanvas.get.cnv.addEventListener('keyup', e => {
 			const code = replaceKeyCode(e.code);
-			this.toggleKey(code, BtnState.Up);
+			this.toggleKeybtn(code, BtnState.Up);
 		});
 		//#endregion
 	}
-	private toggleKey(code: KeyCode, newState: BtnState.Up | BtnState.Down) {
+	private toggleMouseBtn(btnIndex: number, newState: BtnState.Up | BtnState.Down) {
+		if (Input.get._mouse.btn[btnIndex] === undefined)
+			Input.get._mouse.btn[btnIndex] = new Button();
+		Input.get._mouse.btn[btnIndex].toggle(newState);
+	}
+	private toggleKeybtn(code: KeyCode, newState: BtnState.Up | BtnState.Down) {
 		if (Input.get._keys[code] === undefined)
 			Input.get._keys[code] = new Button();
 
 		Input.get._keys[code].toggle(newState);
 	}
-	static mousePos() {
-		return Input.get._mouse.pos.copy();
+
+	//#region Static getters and methods
+	static get mousePos() {
+		return Input.get._mouse.pos;
 	}
-	static mouseWheel() {
-		//? close, but this is faster than the event
-		const wheel = {...Input.get._mouse.wheel} as {x: number, y: number};
-		return wheel;
+	static get mouseIn() {
+		return Input.get._mouse.isInside;
 	}
-	static mouseBtnState(btnIndex: number) {
-		return Input.get._mouse.btn[btnIndex].state;
+	static get mouseWheel() {
+		return Input.get._mouse.wheel;
+	}
+	static get mouseBtn() {
+		return Input.get._mouse.btn;
+	}
+	static get keys() {
+		return Input.get._keys;
 	}
 	static keyState(keyCode: string) {
 		if (Input.get._keys[keyCode] === undefined)
@@ -120,6 +177,7 @@ export class Input extends Singleton {
 
 		return Input.get._keys[keyCode].state;
 	}
+	//#endregion
 }
 //#endregion
 
--- src/test.ts
+++ src/test.ts
@@ -1,10 +1,36 @@
-import { Circle, MainCanvas, Text, getSubStyleValue } from '@gandolphinnn/graphics2';
+import { Circle, Color, Coord, MainCanvas, Mesh, Text, getSubStyleValue } from '@gandolphinnn/graphics2';
+import { arrPivot, objPivot } from '@gandolphinnn/utils'
+import Enumerable from 'linq/linq.js'
 import { Input, BtnState } from './index.js';
 
-function animate(timestamp: number) {
+const c = MainCanvas.get;
+c.writeStyle.mergeFont('50px Arial')
+
+const animate: FrameRequestCallback = (timestamp: number) => {
 	MainCanvas.get.ctx.clearRect(0,0, innerWidth, innerHeight);
 	requestAnimationFrame(animate);
-	new Circle(Input.mousePos(), 5).render();
+	circ.style.mergeFillStyle(Color.byName(Input.mouseIn? 'Black' : 'Red'));
+	
+	mesh.center = Input.mousePos;
+	btnText.content = logBtns();
+	keyText.content = logKeys();
+	console.log(Input.keys); //! keys dont work!!!!
+	
+	mesh.render();
 }
+const circ = new Circle(new Coord(0,0), 5);
+const btnText = new Text(new Coord(0,-90), '');
+const keyText = new Text(new Coord(0,-30), '');
+const mesh = new Mesh(new Coord(0,0), circ, keyText, btnText)
 window.requestAnimationFrame(animate);
+function logBtns() {
+	return JSON.stringify(Enumerable.from(Input.mouseBtn).toArray().map((btn) => {
+		return `${btn.key}, ${BtnState[btn.value.state]}`
+	}));
+}
+function logKeys() {
+	return JSON.stringify(Enumerable.from(Input.keys).toArray().map((btn) => {
+		return `${btn.key}, ${BtnState[btn.value.state]}`
+	}));
+}
 /**/
